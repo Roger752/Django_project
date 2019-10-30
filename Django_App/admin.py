5d5f056327715ed5038e519fc178218081f55d2c
KIM HYEONJIN <polo9562@gmail.com>
2019-10-30 22:00:41 +0900

    admin user adding and model setting

--- Django_App/admin.py
+++ Django_App/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from Django_App.models import Memo
 
 # Register your models here.
+class MemoAdmin(admin.ModelAdmin):
+    list_display = ('content', 'last_modified')
+
+admin.site.register(Django_App, MemoAdmin)
